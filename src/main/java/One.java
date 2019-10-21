6f6dc85ac9cbe656c3a161bce9ba74fc1efc4f5c
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-21 11:27:26 +0400

    -- folder structure

--- src/main/java/One.java
+++
@@ -1,5 +0,0 @@
-public class One {
-  public static void main(String[] args) {
-    System.out.println("Concurrency. Goetz");
-  }
-}
---
+++ src/main/java/concurrency/Welcome.java
@@ -0,0 +1,7 @@
+package concurrency;
+
+public class Welcome {
+  public static void main(String[] args) {
+    System.out.println("Java Concurrency. by Goetz.\nWelcome");
+  }
+}
